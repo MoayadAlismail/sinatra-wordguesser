5058c3f389e80fbc12cab98450880ec4d1b69506
Moayad Alismail <maa9566@nyu.edu>
2025-10-01 00:32:39 +0000

    init

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,17 +1,7 @@
 class WordGuesserGame
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/wordguesser_game_spec.rb pass.
+  # creates getter and setter methods automatically
+  attr_accessor :word, :guesses, :wrong_guesses
 
-  # Get a word from remote "random word" service
-
-  def initialize(word)
-    @word = word
-  end
-
-  # You can test it by installing irb via $ gem install irb
-  # and then running $ irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
-  #  => "cooking"   <-- some random word
   def self.get_random_word
     require 'uri'
     require 'net/http'
@@ -19,5 +9,39 @@ class WordGuesserGame
     Net::HTTP.new('randomword.saasbook.info').start do |http|
       return http.post(uri, "").body
     end
+  end
+
+  def initialize(word)
+    raise ArgumentError, "word must be non-empty" if word.nil? || word.strip.empty?
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter.strip.empty?
+    ch = letter.downcase
+    raise ArgumentError unless ch.match?(/\A[a-z]\z/)
+  
+    # duplicate -> false (ignored)
+    return false if @guesses.include?(ch) || @wrong_guesses.include?(ch)
+  
+    if @word.include?(ch)
+      @guesses << ch
+    else
+      @wrong_guesses << ch
+    end
+      true
+  end
+  
+
+  def word_with_guesses
+    @word.chars.map { |c| @guesses.include?(c) ? c : '-' }.join
+  end
+
+  def check_win_or_lose
+    return :win  if word_with_guesses == @word
+    return :lose if @wrong_guesses.length >= 7
+    :play
   end
 end
